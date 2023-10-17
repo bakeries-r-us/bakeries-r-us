c6933ca00ef270aceb883e50dc3f777f7b75f6af
Peter Ajtai <peter.ajtai@soliddigital.com>
2023-10-17 11:25:33 -0700

    Choices to begin

--- src/levels/Level02.jsx
+++ src/levels/Level02.jsx
@@ -3,7 +3,13 @@ function Level02() {
 
     return (
         <>
-            <p>Congrats!</p>
+            <p>Your first task is to decide what to do first:</p>
+            <div>
+                <input type="radio" name="action" value="Start"/> Start baking bread
+                <input type="radio" name="action" value="Sell"/> Find folks to buy the bread
+                <input type="radio" name="action" value="Delivery"/> Get some delivery trucks
+                <input type="radio" name="action" value="Inventory"/> Take an inventory of what you have
+            </div>
         </>
     )
 }
